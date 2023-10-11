32695a432ee2afd6b16c7b9e9d2cffd29cf93989
Hancie123 <hanciewanemphago@gmail.com>
2023-10-11 22:16:32 +0545

    commit change

--- lib/views/forgotpassword.dart
+++ lib/views/forgotpassword.dart
@@ -9,6 +9,7 @@ class MyForgotPassword extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: Text("Forgot Password"),
       ),
--- lib/views/login/login.dart
+++ lib/views/login/login.dart
@@ -13,6 +13,7 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Column(
         children: [
           Padding(
